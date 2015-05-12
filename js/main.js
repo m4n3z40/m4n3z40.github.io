fb66ea0547e597d54e46ecce90c58ac39a67eabb
Allan Baptista <allan.marques@ymail.com>
2015-05-11 22:56:49 -0300

    Adicionado tipografia e outros pequenos ajustes visuais

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 (function(root, doc, undefined) {
     'use strict';
 
-    function initializeLocationMap() {
-    	var gMaps = root.google.maps,
-	    	mapOptions = {
-	    		center: new gMaps.LatLng(-34.397, 150.644),
-		        zoom: 8,
+    var position = {
+        latitude: -23.5980161,
+        longitude: -46.6413542
+    };
+
+    function initializeLocationMap(gMaps) {
+    	var mapOptions = {
+	    		center: new gMaps.LatLng(position.latitude, position.longitude),
+		        zoom: 16,
 		        mapTypeId: gMaps.MapTypeId.ROADMAP,
 		        disableDefaultUI: true,
                 scrollwheel: false
@@ -14,8 +18,34 @@
     	return new gMaps.Map(doc.getElementById('location-map'), mapOptions);
     }
 
+    function addPlaceMarker(gMaps, map) {
+        return new gMaps.Marker({
+            position: new gMaps.LatLng(position.latitude, position.longitude),
+            map: map,
+            title: 'Nosso endereço'
+        });
+    }
+
+    function addAddresInfoWindow(gMaps, map, marker) {
+        var infoWindow = new gMaps.InfoWindow({
+            content: ['<h4>Nosso Endereço</h4>',
+                      '<address>R. Marselhesa, 658 - ',
+                      'Vila Mariana, SP, 04020-060</address>'].join('')
+        });
+
+        gMaps.event.addListener(marker, 'click', function() {
+            infoWindow.open(map, marker);
+        });
+
+        return infoWindow;
+    }
+
     $(doc).ready(function() {
-    	var locationMap = initializeLocationMap();
+    	var gMaps = root.google.maps,
+            locationMap = initializeLocationMap(gMaps),
+            placeMarker = addPlaceMarker(gMaps, locationMap);
+
+        addAddresInfoWindow(gMaps, locationMap, placeMarker);
     });
 
 })(window, document);
